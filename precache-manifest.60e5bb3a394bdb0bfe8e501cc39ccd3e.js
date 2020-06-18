098b41d2178cf1f5071fe61c5d071a2ca5eef4ce
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-06-18 20:24:01 +0400

    Updates

---
+++ precache-manifest.60e5bb3a394bdb0bfe8e501cc39ccd3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6d6ee55cfc889c5504563723b26a26",
+    "url": "/movieProjectReact/index.html"
+  },
+  {
+    "revision": "6c3bad5881b439802dfc",
+    "url": "/movieProjectReact/static/css/main.66f77dd7.chunk.css"
+  },
+  {
+    "revision": "fb1208d9f89a2ed60ee5",
+    "url": "/movieProjectReact/static/js/2.44fdf448.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieProjectReact/static/js/2.44fdf448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3bad5881b439802dfc",
+    "url": "/movieProjectReact/static/js/main.91df662b.chunk.js"
+  },
+  {
+    "revision": "4346abf18a37b5517608",
+    "url": "/movieProjectReact/static/js/runtime-main.5efa9e93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movieProjectReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
