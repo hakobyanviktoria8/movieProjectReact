a12a59624ad3d1c87c38a7b6d16c4738358aa881
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-01-13 00:56:36 +0400

    finished project

--- src/MovieProject/Home/Print/Movies.js
+++ src/MovieProject/Home/Print/Movies.js
@@ -11,14 +11,14 @@ export function Movies(props) {
     const  request = [
         {title: "Trending", url:`${API_URl}/trending/all/week?api_key=${API_KEY}&language=en-US`},
         {title: "Top Rated", url:`${API_URl}/movie/top_rated?api_key=${API_KEY}&language=en-US`},
-        {title: "Action Movies", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=28&append_to_response=videos`},
-        {title: "Comedy Movies", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=35&append_to_response=videos`},
-        {title: "Romance Movies", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=10749&append_to_response=videos`},
-        {title: "Horror Movies", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=27&append_to_response=videos`},
-        {title: "Mystery", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=9648&append_to_response=videos`},
-        {title: "Scifi", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=878&append_to_response=videos`},
-        {title: "Western", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=37&append_to_response=videos`},
-        {title: "Animation", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=16&append_to_response=videos`},
+        // {title: "Action Movies", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=28&append_to_response=videos`},
+        // {title: "Comedy Movies", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=35&append_to_response=videos`},
+        // {title: "Romance Movies", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=10749&append_to_response=videos`},
+        // {title: "Horror Movies", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=27&append_to_response=videos`},
+        // {title: "Mystery", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=9648&append_to_response=videos`},
+        // {title: "Scifi", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=878&append_to_response=videos`},
+        // {title: "Western", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=37&append_to_response=videos`},
+        // {title: "Animation", url:`${API_URl}/discover/movie/?api_key=${API_KEY}&with_genres=16&append_to_response=videos`},
     ];
 
     //div display block or none
